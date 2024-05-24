4169df38572444cfdfb68322de00287faa34b885
rashiq <rashiqazhan@gmail.com>
2024-05-25 01:47:53 +0600

    Change to ts alias paths.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { TextAnalyzerController } from "./text-analyzer/text-analyzer.controller";
+import { AppController } from "src/app.controller";
+import { AppService } from "src/app.service";
+import { TextAnalyzerController } from "src/text-analyzer/text-analyzer.controller";
+import { TextAnalyzerService } from "src/text-analyzer/text-analyzer.service";
 
 @Module({
   imports: [],
