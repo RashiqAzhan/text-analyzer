01757914cf300c5adf9fad47242571bc27c3d28a
rashiq <rashiqazhan@gmail.com>
2024-05-25 01:54:22 +0600

    Use text analyzer service.

--- src/text-analyzer/text-analyzer.controller.spec.ts
+++ src/text-analyzer/text-analyzer.controller.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
-import { TextAnalyzerController } from "./text-analyzer.controller";
+import { TextAnalyzerController } from "src/text-analyzer/text-analyzer.controller";
+import { TextAnalyzerService } from "src/text-analyzer/text-analyzer.service";
 
 describe("TextAnalyzerController", () => {
   let controller: TextAnalyzerController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [TextAnalyzerService],
       controllers: [TextAnalyzerController],
     }).compile();
 
--- src/text-analyzer/text-analyzer.controller.ts
+++ src/text-analyzer/text-analyzer.controller.ts
@@ -1,9 +1,23 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get, Post } from "@nestjs/common";
+import { TextAnalyzerService } from "src/text-analyzer/text-analyzer.service";
 
 @Controller("text-analyzer")
 export class TextAnalyzerController {
+  constructor(private readonly textAnalyzerService: TextAnalyzerService) {}
+
   @Get()
   getHello(): string {
     return "you've reached text-analyzer";
+  }
+
+  @Post()
+  analyzeText(@Body() body: { text: string }): string {
+    console.log(body);
+    return "you've reached text-analyzer";
+  }
+
+  @Post("word-count")
+  countWords(@Body() body: { text: string }): string {
+    return this.textAnalyzerService.wordCount(body.text);
   }
 }
