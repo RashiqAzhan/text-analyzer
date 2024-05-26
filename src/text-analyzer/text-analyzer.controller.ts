61ccf5629465b5bd33e980650257526b65623da4
RashiqAzhan <rashiqazhan@gmail.com>
2024-05-26 22:05:02 +0600

    Add rate-limiting. (#4)

--- src/text-analyzer/text-analyzer.controller.ts
+++ src/text-analyzer/text-analyzer.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Get, Post } from "@nestjs/common";
+import { SkipThrottle, Throttle } from "@nestjs/throttler";
 import { TextAnalyzerService } from "src/text-analyzer/text-analyzer.service";
 
 @Controller("text-analyzer")
 export class TextAnalyzerController {
   constructor(private readonly textAnalyzerService: TextAnalyzerService) {}
 
+  @SkipThrottle({ short: true, medium: true })
+  @Throttle({ long: { limit: 1000, ttl: 60000 } })
   @Get()
   getLanding(): string {
     return "Welcome to text-analyzer!";
